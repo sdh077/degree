7adf718fd99ef693fb8262b9a9bafe5887572d20
shindaeho <sdh077@naver.com>
2023-10-30 21:06:29 +0900

    local

--- app/(ins)/page.tsx
+++ app/(ins)/page.tsx
@@ -1,19 +1,10 @@
+import CardWrap from '@/components/main/card-wrap'
 export const metadata = {
   title: 'Home - Creative',
   description: 'Page description',
 }
-
-import CardWrap from '@/components/main/card-wrap'
-async function getData(ins) {
-  const res = await fetch(`http://localhost:3000/api/items`, { cache: 'no-store' })
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  return res.json()
-}
 export default async function Home({ params }) {
-  const data = await getData(params.ins)
+  const data = []
   return (
     <>
       {/* <Hero /> */}
